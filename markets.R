e833bc0dbebfc8aae11dbf8666b977ee4e41507e
NickyDy <nickydyakov@gmail.com>
2025-11-25 16:46:20 +0200

    new commit

--- markets.R
+++ markets.R
@@ -8,7 +8,7 @@ read_delim_cc <- function(file) {
   read_delim(file, col_types = "cccccdd")
 }
 
-url <- "https://kolkostruva.bg/opendata_files/2025-11-23.zip"
+url <- "https://kolkostruva.bg/opendata_files/2025-11-24.zip"
 destfile <- tempfile(pattern = 'markets', tmpdir = tempdir(), fileext = '.zip')
 download.file(url, destfile, quiet = T)
 zip <- dir_ls(tempdir(), glob = "*.zip")
@@ -30,7 +30,7 @@ markets <- map(files, read_delim_cc) %>%
   mutate(cena_v_promocia = as.character(cena_v_promocia),
          cena_v_promocia = str_replace(cena_v_promocia, "NaN", ""),
          naimenovanie_na_produkta = reorder_within(naimenovanie_na_produkta, cena_na_drebno, market),
-         date = "2025-11-23", .before = everything())
+         date = "2025-11-24", .before = everything())
 
 df_markets <- read_parquet("markets/df_markets.parquet")
 df_markets <- bind_rows(df_markets, markets)
@@ -38,10 +38,10 @@ df_markets <- bind_rows(df_markets, markets)
 write_parquet(df_markets, "markets/df_markets.parquet")
 
 glimpse(df_markets)
-df_markets %>% count(date)
+df_markets %>% count(date) %>% print(n = Inf)
 
 df_markets %>% 
-  filter(date == "2025-11-23", str_detect(naimenovanie_na_produkta, 
+  filter(date == "2025-11-24", str_detect(naimenovanie_na_produkta, 
                                           regex("^(?=.*ябълки)(?=.*кг).*$", ignore_case = T))) %>%
   ggplot(aes(cena_na_drebno, naimenovanie_na_produkta, fill = market)) +
   geom_col(show.legend = F) +
@@ -67,23 +67,7 @@ df_markets %>%
   scale_x_date(date_breaks = "3 days", date_labels = "%b-%d") +
   scale_color_manual(values = c("black", "red"), 
                      labels = c("Цена на дребно", "Цена в промоция")) +
-  theme(text = element_text(size = 14), legend.position = "top",
+  theme(text = element_text(size = 16), legend.position = "top",
         strip.text = element_markdown()) +
   labs(y = "Цена (лв)", x = "Дата", color = "Легенда:") +
   facet_wrap(vars(market_product), ncol = 4)
-
-# billa <- read_csv("https://kolkostruva.bg/upload/11978/import.csv", col_types = c("cccccdd")) %>% 
-#   mutate(market = "Била", .before = everything())
-# vilton <- read_csv("https://kolkostruva.bg/upload/11933/import.csv", col_types = c("cccccdd")) %>% 
-#   mutate(market = "Вилтон", .before = everything())
-# kaufland <- read_csv("https://kolkostruva.bg/upload/11988/import.csv", col_types = c("cccccdd")) %>% 
-#   mutate(market = "Кауфланд", .before = everything())
-# lidl <- read_csv("https://kolkostruva.bg/upload/11943/import.csv", col_types = c("cccccdd")) %>% 
-#   mutate(market = "Лидл", .before = everything())
-# slaveks <- read_csv("https://kolkostruva.bg/upload/11561/PriceExportKzp_2025_11_03.csv", col_types = c("cccccdd")) %>% 
-#   mutate(market = "Славекс", .before = everything())
-# tmarket <- read_csv("https://kolkostruva.bg/upload/11583/KZP_03.11.2025.csv", col_types = c("cccccdd")) %>% 
-#   mutate(market = "Т Маркет", .before = everything())
-
-# markets <- bind_rows(billa, vilton, kaufland, lidl, slaveks, tmarket) %>% janitor::clean_names() %>% 
-#   mutate(cena_v_promocia = na_if(cena_v_promocia, 0))
